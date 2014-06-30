melisa-bok-olx/akka-trainning

Initial state (koan:initial)

src/main/scala/com/typesafe/training/hakkyhour/Terminal.scala
@@ -1,0 +1,61 @@
+/*
+ * Copyright © 2014 Typesafe, Inc. All rights reserved.
+ */
+
+package com.typesafe.training.hakkyhour
+
+import scala.util.parsing.combinator.RegexParsers
+
+trait Terminal {
+
+  sealed trait Command
+
+  object Command {
+
+    case class CreateGuest(count: Int, drink: Drink, isStubborn: Boolean, maxDrinkCount: Int) extends Command
+
+    case object GetStatus extends Command
+
+    case object Quit extends Command
+
+    case class Unknown(command: String) extends Command
+
+    def apply(command: String): Command =
+      CommandParser.parseAsCommand(command)
+  }
+
+  object CommandParser extends RegexParsers {
+
+    val commandParser: Parser[Command] =
+      createGuest | getStatus | quit
+
+    def parseAsCommand(s: String): Command =
+      parseAll(commandParser, s) match {
+        case Success(command, _) => command
+        case _                   => Command.Unknown(s)
+      }
+
+    def createGuest: Parser[Command.CreateGuest] =
+      opt(int) ~ ("guest|g".r ~> opt(drink) ~ opt("s".r) ~ opt(int)) ^^ {
+        case count ~ (drink ~ stubborn ~ maxDrinkCount) =>
+          Command.CreateGuest(
+            count getOrElse 1,
+            drink getOrElse Drink.Akkarita,
+            stubborn.isDefined,
+            maxDrinkCount getOrElse Int.MaxValue
+          )
+      }
+
+    def getStatus: Parser[Command.GetStatus.type] =
+      "status|s".r ^^ (_ => Command.GetStatus)
+
+    def quit: Parser[Command.Quit.type] =
+      "quit|q".r ^^ (_ => Command.Quit)
+
+    def drink: Parser[Drink] =
+      "A|a|M|m|P|p".r ^^ Drink.apply
+
+    def int: Parser[Int] =
+      """\d+""".r ^^ (_.toInt)
+  }
+}

src/test/scala/com/typesafe/training/hakkyhour/DrinkSpec.scala
@@ -1,0 +1,35 @@
+/*
+ * Copyright © 2014 Typesafe, Inc. All rights reserved.
+ */
+
+package com.typesafe.training.hakkyhour
+
+import org.scalatest.{ Matchers, Inspectors, WordSpec }
+
+class DrinkSpec extends WordSpec with Matchers with Inspectors {
+
+  import Drink._
+
+  "drinks" should {
+    "contain Akkarita, MaiPlay and PinaScalada" in {
+      drinks shouldEqual Set(Akkarita, MaiPlay, PinaScalada)
+    }
+  }
+
+  "Calling apply" should {
+    "create the correct Drink for the given code" in {
+      apply("A") shouldEqual Akkarita
+      apply("a") shouldEqual Akkarita
+      apply("M") shouldEqual MaiPlay
+      apply("m") shouldEqual MaiPlay
+      apply("P") shouldEqual PinaScalada
+      apply("p") shouldEqual PinaScalada
+    }
+  }
+
+  "Calling anyOther" should {
+    "return an other Drink than the given one" in {
+      forAll(drinks) { drink => anyOther(drink) should not equal drink }
+    }
+  }
+}

edwardsoo/rail

src/main/scala/com/typesafe/training/hakkyhour/Terminal.scala
@@ -1,0 +1,61 @@
+/*
+ * Copyright © 2014 Typesafe, Inc. All rights reserved.
+ */
+
+package com.typesafe.training.hakkyhour
+
+import scala.util.parsing.combinator.RegexParsers
+
+trait Terminal {
+
+  sealed trait Command
+
+  object Command {
+
+    case class CreateGuest(count: Int, drink: Drink, isStubborn: Boolean, maxDrinkCount: Int) extends Command
+
+    case object GetStatus extends Command
+
+    case object Quit extends Command
+
+    case class Unknown(command: String) extends Command
+
+    def apply(command: String): Command =
+      CommandParser.parseAsCommand(command)
+  }
+
+  object CommandParser extends RegexParsers {
+
+    def parseAsCommand(s: String): Command =
+      parseAll(parser, s) match {
+        case Success(command, _) => command
+        case _                   => Command.Unknown(s)
+      }
+
+    def createGuest: Parser[Command.CreateGuest] =
+      opt(int) ~ ("guest|g".r ~> opt(drink) ~ opt("s".r) ~ opt(int)) ^^ {
+        case count ~ (drink ~ stubborn ~ maxDrinkCount) =>
+          Command.CreateGuest(
+            count getOrElse 1,
+            drink getOrElse Drink.Akkarita,
+            stubborn.isDefined,
+            maxDrinkCount getOrElse Int.MaxValue
+          )
+      }
+
+    def getStatus: Parser[Command.GetStatus.type] =
+      "status|s".r ^^ (_ => Command.GetStatus)
+
+    def quit: Parser[Command.Quit.type] =
+      "quit|q".r ^^ (_ => Command.Quit)
+
+    def drink: Parser[Drink] =
+      "A|a|M|m|P|p".r ^^ Drink.apply
+
+    def int: Parser[Int] =
+      """\d+""".r ^^ (_.toInt)
+  }
+
+  val parser: CommandParser.Parser[Command] =
+    CommandParser.createGuest | CommandParser.getStatus | CommandParser.quit
+}

src/test/scala/com/typesafe/training/hakkyhour/DrinkSpec.scala
@@ -1,0 +1,38 @@
+/*
+ * Copyright © 2014 Typesafe, Inc. All rights reserved.
+ */
+
+package com.typesafe.training.hakkyhour
+
+import org.scalatest.{ Matchers, Inspectors, WordSpec }
+
+class DrinkSpec extends WordSpec with Matchers with Inspectors {
+
+  import Drink._
+
+  "drinks" should {
+    "contain Akkarita, MaiPlay and PinaScalada" in {
+      drinks shouldEqual Set(Akkarita, MaiPlay, PinaScalada)
+    }
+  }
+
+  "Calling apply" should {
+    "create the correct Drink for a known code" in {
+      apply("A") shouldEqual Akkarita
+      apply("a") shouldEqual Akkarita
+      apply("M") shouldEqual MaiPlay
+      apply("m") shouldEqual MaiPlay
+      apply("P") shouldEqual PinaScalada
+      apply("p") shouldEqual PinaScalada
+    }
+    "throw an IllegalArgumentException for an unknown code" in {
+      an[IllegalArgumentException] should be thrownBy apply("1")
+    }
+  }
+
+  "Calling anyOther" should {
+    "return an other Drink than the given one" in {
+      forAll(drinks) { drink => anyOther(drink) should not equal drink }
+    }
+  }
+}